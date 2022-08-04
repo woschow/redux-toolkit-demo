44bb195f5b1e18d3f7388befe476932f21b4263e
Andrej Woschow <Andrej.Woschow@msg-services.de>
2022-08-04 16:50:43 +0200

    Added components

---
+++ src/webparts/reduxToolkitDemo/components/pages/posts/PostItem.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const PostItem = () => {
+    return (
+        <div>Post item</div>
+    );
+}
+
+export default PostItem;
+
---
+++ src/webparts/reduxToolkitDemo/components/pages/users/UsersContainer.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const UsersContainer = () => {
+
+    console.log('Users')
+    return (
+        <h2>Users</h2>
+    );
+}
+
+export default UsersContainer;
